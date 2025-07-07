109692c8ce040dd23d71c19e5c0f383637e8e8d4
iosxx <51318333+iosxx@users.noreply.github.com>
2025-07-08 00:42:41 +0800

    Initial commit

---
+++ elog/elog.config.yuque-pwd.js
@@ -0,0 +1,77 @@
+module.exports = {
+  write: {
+    platform: 'yuque-pwd',
+    "yuque-pwd": {
+      username: process.env.YUQUE_USERNAME,
+      password: process.env.YUQUE_PWD,
+      host: '',
+      login: process.env.YUQUE_LOGIN,
+      repo: process.env.YUQUE_REPO,
+      linebreak: false
+    }
+  },
+  
+  deploy: {
+    platform: "local",
+    local: {
+      outputDir: "./source/_posts/语雀/",
+      filename: "title",
+      format: "matter-markdown",
+      catalog: true,
+      formatExt: "",
+    }
+  },
+  
+  image: {
+    enable: true,
+    platform: 'local',
+    local: {
+      outputDir: './source/_posts/img/',
+      prefixKey: 'https://jsd.cdn.zzko.cn/gh/iosxx/blog-butterfly@posts/img/',
+    },
+    oss: {
+      secretId: process.env.OSS_SECRET_ID,
+      secretKey: process.env.OSS_SECRET_KEY,
+      bucket: process.env.OSS_BUCKET,
+      region: process.env.OSS_REGION,
+      host: process.env.OSS_HOST,
+      prefixKey: '',
+      secretExt: '',
+    },
+    cos: {
+      secretId: process.env.COS_SECRET_ID,
+      secretKey: process.env.COS_SECRET_KEY,
+      bucket: process.env.COS_IMAGE_BUCKET,
+      region: process.env.COS_IMAGE_REGION,
+      host: process.env.COS_HOST,
+      prefixKey: 'elog-docs-images',
+      secretExt: '',
+    },
+    qiniu: {
+      secretId: process.env.QINIU_SECRET_ID,
+      secretKey: process.env.QINIU_SECRET_KEY,
+      bucket: process.env.QINIU_BUCKET,
+      region: process.env.QINIU_REGION,
+      host: process.env.QINIU_HOST,
+      prefixKey: '',
+      secretExt: '',
+    },
+    upyun: {
+      user: process.env.UPYUN_USER,
+      password: process.env.UPYUN_PASSWORD,
+      bucket: process.env.UPYUN_BUCKET,
+      host: process.env.UPYUN_HOST,
+      prefixKey: '',
+      secretExt: '',
+    },
+    github: {
+      user: process.env.GITHUB_USER,
+      token: process.env.GITHUB_TOKEN,
+      repo: process.env.GITHUB_REPO,
+      branch: '',
+      host: '',
+      prefixKey: '',
+      secretExt: '',
+    }
+  }
+}
